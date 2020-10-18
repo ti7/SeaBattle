996d0a962781cca680dd4af89e9769bfec40e872
Tim <45114854+ti7@users.noreply.github.com>
2020-10-18 14:59:56 +0500

    Ручная, автоматическая и ручная установка, меню программы.

--- SeaBattle/SeaBattle.cpp
+++ SeaBattle/SeaBattle.cpp
@@ -9,10 +9,9 @@
 using namespace std;
 
 const int SZ = 10;
-
+const int NUMSHIPS = 9;
 int shipsId = 1;
-
-int ships[10] = {0};
+int ships[10] = { 0 };
 
 void gotoxy(int x, int y)
 {
@@ -75,7 +74,7 @@ bool shipInMap(int x, int y, int dir, int sizeShip)
 	return inMap;
 }
 
-bool setShip(int map[SZ][SZ], int x, int y, int dir, int sizeShip)
+bool setShip(int map[SZ][SZ], int x, int y, int dir, int sizeShip) 
 {
 	int temp_x = x;
 	int temp_y = y;
@@ -221,14 +220,14 @@ bool setShip(int map[SZ][SZ], int x, int y, int dir, int sizeShip)
 	return settinIsPossible;
 }
 
-void setShipRandom(int map[SZ][SZ], int sizeShip, int numShips)
+void setShipRandom(int map[SZ][SZ], int sizeShip, int shipsIdNew) // Установко кораблей автоматически рандомно
 {
 	int x, y;
 	int dir = 0;
 	int countShip = 0;
 	int countTact = 0;
 
-	while (countShip < numShips)
+	while (countShip < 1)
 	{
 		countTact++;
 		if (countTact > 1000)
@@ -241,9 +240,7 @@ void setShipRandom(int map[SZ][SZ], int sizeShip, int numShips)
 		int temp_y = y;
 
 		bool settinIsPossible = 1;
-
-		//dir = rand() % 4;
-
+				
 		for (int i = 0; i < sizeShip; i++)
 		{
 			if (x < 0 || y < 0 || x >= SZ || y >= SZ)
@@ -289,7 +286,7 @@ void setShipRandom(int map[SZ][SZ], int sizeShip, int numShips)
 
 			for (int i = 0; i < sizeShip; i++)
 			{
-				map[x][y] = shipsId;
+				map[x][y] = shipsIdNew;
 
 				switch (dir)
 				{
@@ -307,14 +304,15 @@ void setShipRandom(int map[SZ][SZ], int sizeShip, int numShips)
 					break;
 				}
 			}
-			ships[shipsId] = sizeShip;
-			shipsId++;
+			/*ships[shipsId] = sizeShip;*/
+
+			/*shipsId++;*/
 			countShip++;
 		}
 	}
 }
 
-void mapShow(int map[SZ][SZ], int mask[SZ][SZ])
+void mapShow(int map[SZ][SZ], int mask[SZ][SZ], bool useMask) // Показать поле
 {
 	cout << ' ';
 	for (int i = 0; i < SZ; i++)
@@ -327,9 +325,10 @@ void mapShow(int map[SZ][SZ], int mask[SZ][SZ])
 	for (int i = 0; i < SZ; i++)
 	{
 		cout << i;
+
 		for (int j = 0; j < SZ; j++)
 		{
-			// if (mask[j][i] == 1) // Вкл, выкючить туман войны
+			//if (mask[j][i] == 1 || useMask == 0) // Вкл, выкючить туман войны
 			{
 				if (map[j][i] == 0)
 				{
@@ -339,6 +338,11 @@ void mapShow(int map[SZ][SZ], int mask[SZ][SZ])
 				{
 					cout << 'X';
 				}
+
+				else if (map[j][i] == -2)
+				{
+					cout << '.';
+				}
 					
 				else
 				{
@@ -347,34 +351,59 @@ void mapShow(int map[SZ][SZ], int mask[SZ][SZ])
 			}
 			//else
 			//{
-			//	cout << ' ';   // Вкл, выкючить туман войны
+			//cout << ' ';   // Вкл, выкючить туман войны
 			//} 
 		}
 		cout << endl;
 	}
 }
 
-int main()
-
+int shoot(int map[SZ][SZ], int x, int y, int ships[NUMSHIPS+1], int mask[SZ][SZ])
 {
-	setlocale(LC_CTYPE, "Russian");
-	
-	int map[SZ][SZ] = {0};
-	int mask[SZ][SZ] = {0};
+	int result = 0;
 
-	/*setShipRandom(map, 4, 1); 
-	setShipRandom(map, 3, 2);
-	setShipRandom(map, 2, 3);
-	setShipRandom(map, 1, 4);*/
-			
-	int x = 0, y = 0;
-	int dir = 1;
-	int sizeShip = 4;
-	int ch;
-
-	while (true)
+	if (map[x][y] >= 1)
 	{
-		mapShow(map, mask);
+		ships[map[x][y]]--;
+
+		if (ships[map[x][y]] <= 0)
+		{
+			result = 2;
+			
+		}
+		else
+		{
+			result = 1;
+			
+		}
+
+		map[x][y] = -1;
+
+	}
+
+	else
+	{
+		map[x][y] = -2;
+	}
+		
+	mask[x][y] = 1;
+
+	return result;
+
+}
+
+void setShipsPlayers(int map[SZ][SZ], int sizeShip, int shipsIdNew)
+{
+	int dir = 1;
+	int ch;
+	int x = 0;
+	int y = 0;
+	int mask[SZ][SZ];
+	int numberOfShips = 1;
+		
+	while (numberOfShips < 2)
+	{
+		mapShow(map, mask, 0);;
 		shipShow(x, y, dir, sizeShip);
 		
 		int temp_x = x;
@@ -405,7 +434,8 @@ int main()
 				{
 					x = 0, y = 0;
 					dir = 1;
-					sizeShip--;
+					/*sizeShip--;*/
+					numberOfShips++;
 				}
 				break;
 			}
@@ -415,67 +445,135 @@ int main()
 			x = temp_x;
 			y = temp_y;
 			dir = temp_dir;
+			numberOfShips++;
 		}
 
 		system("cls");
 		
+
 	}
+	
+}
 
-	while (true)
+int main()
+
+{
+	setlocale(LC_CTYPE, "Russian");
+	
+	int paramOne = 0;
+	int paramTwo = 0;
+
+	int map1[SZ][SZ] = { 0 };
+	int map2[SZ][SZ] = { 0 };
+
+	int ships1[NUMSHIPS + 1] = { 0, 4, 3, 3, 2, 2, 2, 1, 1, 1 }; // Жизни кораблей первого поля
+	int ships2[NUMSHIPS + 1] = { 0, 4, 3, 3, 2, 2, 2, 1, 1, 1 }; // Жизни кораблей второго поля
+
+	int mask1[SZ][SZ] = { 0 };
+	int mask2[SZ][SZ] = { 0 };
+
+		cout << "_____________________¶____________¶¶¶¶____________" << endl;
+		cout << "__________________¶¶¶¶¶¶¶_________¶_______________" << endl;
+		cout << "__________________¶__¶__¶_______¶¶¶¶¶_____________" << endl;
+		cout << "____________¶_____¶__¶__¶_______¶_¶_¶_____________" << endl;
+		cout << "____________¶¶___¶¶¶¶¶¶¶¶¶_____¶¶¶¶¶¶¶____________" << endl;
+		cout << "____¶¶______¶¶¶__¶___¶___¶_____¶__¶__¶____________" << endl;
+		cout << "____¶¶¶______¶___¶___¶____¶___¶___¶___¶___________" << endl;
+		cout << "____¶¶¶¶__¶¶¶¶__¶___¶¶¶___¶__¶____¶____¶__________" << endl;
+		cout << "____¶¶¶¶¶__¶¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶__________" << endl;
+		cout << "_____¶__¶¶__¶¶¶______¶______¶_____¶____¶____¶¶_¶__" << endl;
+		cout << "_____¶¶¶¶____¶_______¶______¶_____¶_____¶__¶¶¶¶___" << endl;
+		cout << "___¶¶¶¶¶¶¶___¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶__" << endl;
+		cout << "___¶¶¶¶¶¶¶¶¶¶¶¶¶_____¶_________¶¶¶¶¶¶¶__¶¶¶__¶¶¶__" << endl;
+		cout << "____¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶___¶¶¶__¶¶¶¶¶¶¶¶¶¶¶_________" << endl;
+		cout << "____¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶___________" << endl;
+		cout << "____¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_____________" << endl;
+
+
+
+	cout << "Добро пожаловать на морское поле битвы, моряк!" << endl;
+	cout << "Введите - '1' если будете играть с компьютером, или '2' если будет играть компьютор против компьютера: " << endl;
+	cin >> paramOne;
+	cout << "Здравствуйте, введте '1' если хотите ввести корабли на поле игрока вручную, или '2' для автоматической расстановки: " << endl;
+	cin >> paramTwo;
+
+	if (paramOne == 1 && paramTwo == 1)
 	{
-		mapShow(map, mask);
-		
-		cout << "Введите коардинаты цела: " << endl;
-		cin >> x;
-		cin >> y;
-
-		if (map[x][y] >= 1)
+		for (int i = 1; i <= NUMSHIPS; i++) // Ручная расстанова кораблей на поле игока
 		{
-			ships[map[x][y]]--;
+			setShipsPlayers(map1, ships1[i], i);
+		}
+	
+		for (int i = 1; i <= NUMSHIPS; i++) // Авоматическая расстановка кораблей на поле ИИ
+		{
+			setShipRandom(map2, ships2[i], i);
+		}
+	}
+	
+	else if (paramOne == 1 && paramTwo == 2)
+	{
+		for (int i = 1; i <= NUMSHIPS; i++)  // Автоматическая расстановка кораблей на поле игрока
+		{
+			setShipRandom(map1, ships1[i], i);
+		}
+
+		for (int i = 1; i <= NUMSHIPS; i++) // Авоматическая расстановка кораблей на поле ИИ
+		{
+			setShipRandom(map2, ships2[i], i);
+		}
+
+	}
+	
+	
+				
+	int x = 0, y = 0;
+	
+	bool turn = 1; //  Флаг выбора чей ход
+
+	while (true) // Главный цикл игры
+	{
+		int resultShoot = 0;
+
+		do 
+		{
+			mapShow(map1, mask1, 0);
+			cout << endl;
+			mapShow(map2, mask1, 1);
 			
-			if (ships[map[x][y]] <= 0)
+			if (turn == 1)
 			{
-				cout << "Потоплен" << endl;
+				cout << endl;
+				cout << "Введите коардинаты цела: " << endl;
+				cin >> x;
+				cin >> y;
+				resultShoot = shoot(map2, x, y, ships2, mask2);
+				cout << resultShoot;
 			}
 			else
 			{
-				cout << "Поврежден" << endl;
-			}
-				
-			map[x][y] = -1;
-
-			bool detectShip = false;
-
-			for (int i = 0; i < SZ; i++)
-			{
-				for (int j = 0; j < SZ; j++)
-				{
-					if (map[i][j] >= 1)
-					{
-						detectShip = true;
-					}
-				}
+				cout << endl;
+				cout << "Ход компьютера" << endl;
+				Sleep(2000);
+				x = rand() % SZ;
+				y = rand() % SZ;
+				resultShoot = shoot(map1, x, y, ships1, mask1);
+				cout << resultShoot;
 			}
 
-			if (detectShip == false) 
-			{
-				cout << "Вы победили!" << endl;
-				break;
-			}
-		}
+			if (resultShoot == 1) cout << "Попал" << endl;
+			else if (resultShoot == 2) cout << "Потоплен" << endl;
+			else cout << "Промах" << endl;
+			
+			Sleep(2000);
+			system("cls");
 
-		else 
-		{
-			cout << "Промах" << endl;
-		}
-				
-		mask[x][y] = 1;
+		} while (resultShoot != 0);
 		
-		Sleep(1000);
-		system("cls");
-		_getch();
+		turn = !turn;
 	}
-	system("pause");
-	return 0;
+	
+	_getch();
+	system("cls");
+	
 	
 }
